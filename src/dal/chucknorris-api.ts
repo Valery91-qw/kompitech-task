e715b780b18f1364d2d74da2d4e1d69df788bf76
Valery91-qw <vakriv91@gmail.com>
2021-11-03 19:08:02 +0300

    feat: described the type of response

--- src/dal/chucknorris-api.ts
+++ src/dal/chucknorris-api.ts
@@ -2,6 +2,18 @@ import axios from 'axios';
 
 export const chucknorrisApi = {
     getRandomJoke() {
-        return axios.get(`https://api.chucknorris.io/jokes/random`)
+        return axios.get<RandomJokeType>(`https://api.chucknorris.io/jokes/random`)
     }
+}
+
+
+
+type RandomJokeType = {
+    categories: Array<string>,
+    created_at: Date,
+    icon_url: string
+    id: string
+    updated_at: Date
+    url: string
+    value: string
 }
